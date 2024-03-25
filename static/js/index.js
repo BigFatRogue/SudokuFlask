02a44b8b1044d26c4d463843ca5d118417f6ec4b
p.golubev <bobik0193@mail.ru>
2024-03-25 18:27:55 +0300

    Init

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,24 @@
+function mousePageXY(e)
+	{
+	  var x = 0, y = 0;
+
+	  if (!e) e = window.event;
+
+	  if (e.pageX || e.pageY)
+	  {
+	    x = e.pageX;
+	    y = e.pageY;
+	  }
+	  else if (e.clientX || e.clientY)
+	  {
+	    x = e.clientX + (document.documentElement.scrollLeft || document.body.scrollLeft) - document.documentElement.clientLeft;
+	    y = e.clientY + (document.documentElement.scrollTop || document.body.scrollTop) - document.documentElement.clientTop;
+	  }
+
+	  return {"x":x, "y":y};
+	}
+
+
 function create_table(row, col) {
     for (let r = 0; r < row; r++) {
         let tr = document.createElement('tr')
@@ -22,12 +43,22 @@ function create_table(row, col) {
                     for (let y = 0; y < row; y++) table.rows[y].cells[c].classList.remove('focus-col')
                 }
             }
-            // td.onclick = (event) =>{
-            //     change_cell = true
-            //     change_cell_address['x'] = c
-            //     change_cell_address['y'] = r
-            //     td.classList.add('change')
-            // }
+            td.onclick = (event) =>{
+                let popup = document.getElementById('popup')
+                popup.style.visibility = 'visible'
+
+                console.log(td.getBoundingClientRect())
+
+                let x = td.getBoundingClientRect().x + 50
+                let y = td.getBoundingClientRect().top + 50
+                popup.style.left = x + 'px'
+                popup.style.top = y + 'px'
+
+                // change_cell = true
+                // change_cell_address['x'] = c
+                // change_cell_address['y'] = r
+                // td.classList.add('change')
+            }
             tr.appendChild(td)
         }
         if (r === 2 || r === 5) tr.className = 'tr-bold'
